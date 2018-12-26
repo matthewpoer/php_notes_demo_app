0a669d12b4a8f4d5e67dc0cdba21609684e78219
Matthew Poer <matthewpoer@gmail.com>
2018-12-26 16:52:47 -0500

    Enhance and Consolidate Demo Data

---
+++ chartreuse.md
@@ -0,0 +1,61 @@
+# (You Dyed Your Hair) Chartreuse
+You're a freckled gal  
+You're a pug-nosed cutie  
+Sweet as Charlotte russe  
+You've got big blue eyes  
+So I ask you why  
+Have you dyed your hair chartreuse?  
+
+Chartreuse (chartreuse)  
+Chartreuse (chartreuse)  
+Though you think it's mighty cute  
+Just wait 'til I write and tell your ma  
+That you dyed your hair chartreuse  
+
+Chartreuse (chartreuse)  
+Chartreuse (chartreuse)  
+Though you think it's mighty cute  
+Just wait 'til I write and tell your ma  
+That you dyed your hair chartreuse  
+
+In the days of old  
+When the knights were bolder  
+And the girls were truer blue  
+Just think what paw  
+Would have said to maw  
+Had she dyed her hair chartreuse  
+
+Chartreuse (chartreuse)  
+Chartreuse (chartreuse)  
+Though you think it's mighty cute  
+You went too far in that beauty booth  
+When you dyed your hair chartreuse  
+
+[Instrumental Break]  
+
+Now you know I know  
+That your hair was black when we lived on Chestnut Street  
+When you wore pigtails  
+And ginger ale was your most favorite treat  
+
+You're a big girl now  
+So you think it's cuter being fast and fancy-loose  
+But you went too far in that beauty booth  
+When you dyed your hair chartreuse  
+
+Chartreuse (chartreuse)  
+Chartreuse (chartreuse)  
+Though you think it's mighty cute  
+Just wait 'til I write and tell your ma  
+That you dyed your hair chartreuse  
+
+Chartreuse (chartreuse)  
+Chartreuse (chartreuse)  
+Though you think it's mighty cute  
+But just wait 'til I write and tell your ma  
+You didn't like black, you didn't like red  
+You hated blondes, well, it's no use  
+You got mad and dyed your hair  
+Chartreuse
+
+[genius.com](https://genius.com/Louis-jordan-you-dyed-your-hair-chartreuse-lyrics)
--- class.database.php
+++ class.database.php
@@ -18,12 +18,26 @@ class Database extends SQLite3 {
   }
 
   public function install_demo_data() {
-    $query = 'insert into notes (id, touched, color, subject, content) values' . PHP_EOL
-      . ' (1, \'1544582405\', \'#000000\', \'Koala Fact #1\', \'No koala has the same fingerprints as another.\'), ' . PHP_EOL
-      . ' (2, \'1544582404\', \'#FF0000\', \'Koala Fact #2\', \'There are around 700 of species of eucalyptus, but koalas are picky eaters. They will only eat about 30 species.\'), ' . PHP_EOL
-      . ' (3, \'1544582403\', \'#0000FF\', \'Koala Fact #3\', \'Koalas are born the size of a jellybean.\'), ' . PHP_EOL
-      . ' (4, \'1544582402\', \'#800080\', \'Koala Fact #4\', \'Koalas are sometimes called "koala bears," but they are not actually related to bears. They are marsupials, so are more closely related to the opossum.\'), ' . PHP_EOL
-      . ' (5, \'1544582401\', \'#00FF00\', \'Koala Fact #5\', \'A diet of eucalyptus is not very nutritious, which is why koalas sleep as many as 20 hours per day.\') ';
+    $query = 'insert into notes (id, touched, color, subject, content) values' . PHP_EOL;
+    $query .= '(1, \'1544582405\', \'#00FF00\', \'Reasons to Hire Matt Poer\', \'1. knows markdown
+2. made this note-taking app [just for you](https://stackoverflow.com/jobs/204945/lead-full-stack-developer-brandboom)
+  * the note-taking app uses PHP, SQL Lite (because MySQL seemed a little heavy here), jQuery and [is available for review or cloning on Github](https://github.com/matthewpoer/php_notes_demo_app).
+3. can tell you a lot about fun animals and a little about classic jazz and R&B\'),' . PHP_EOL;
+    $query .= '(2, \'1544582404\', \'#00FFFF\', \'Koala Facts\', \'* No koala has the same fingerprints as another.
+* There are around 700 of species of eucalyptus, but koalas are picky eaters. They will only eat about 30 species.
+* Koalas are born the size of a jellybean.
+* Koalas are sometimes called "koala bears," but they are not actually related to bears. They are marsupials, so are more closely related to the opossum.
+* A diet of eucalyptus is not very nutritious, which is why koalas sleep as many as 20 hours per day.\'),' . PHP_EOL;
+
+    // Editors like Atom.io will probably try to strip the spaces off the end of
+    // each line of markdown content, even if it is there intentionally to
+    // preserve desired newlines. We'll keep our lyrical example in its own file
+    // to try to prevent that. We have to make sure we load the contents,
+    // respecting all characters, but then swap single quotes for doubled single
+    // quotes
+    $chartreuse = file_get_contents('chartreuse.md');
+    $chartreuse = trim(str_replace("'", "''", $chartreuse));
+    $query .= "(3, '1544582403', '#7FFF00', 'Louis Jordan', '{$chartreuse}')" . PHP_EOL;
     $this->query($query);
   }
 
